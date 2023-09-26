8adb6058de8050553071bf7f57153b581a08bcae
ChrisOVS <oscar.epos@gmail.com>
2023-09-25 21:47:23 -0400

    xxx

--- ChatGPT_Experiments/01_PracticeProject/0.packages.R
+++ ChatGPT_Experiments/01_PracticeProject/0.packages.R
@@ -2,7 +2,7 @@ w_dr <- paste0("C:/Users/Chris van Schalkwyk/Desktop/RStudio_GitHub/RStudio-Pers
 project_dr <- paste0(w_dr, "ChatGPT_Experiments/01_PracticeProject/")
 data_path <- paste0("C:/Users/Chris van Schalkwyk/Desktop/RStudio_GitHub/Data/01_PracticeProject/")
 # List of packages you want to load
-packages <- c("tidyverse", "fst")
+packages <- c("tidyverse", "fst", "plotly")
 
 # Use lapply to load each package
 lapply(packages, library, character.only = TRUE)
