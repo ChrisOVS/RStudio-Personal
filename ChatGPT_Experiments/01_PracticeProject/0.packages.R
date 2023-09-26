d0d9dfa088b71a220c33252e257cc6693da329a5
ChrisOVS <oscar.epos@gmail.com>
2023-09-25 20:15:05 -0400

    New changes, got data in etc.

--- ChatGPT_Experiments/01_PracticeProject/0.packages.R
+++ ChatGPT_Experiments/01_PracticeProject/0.packages.R
@@ -1,6 +1,6 @@
-w_dr <- getwd()
-project_dr <- paste0(w_dr, "/ChatGPT_Experiments/01_PracticeProject/")
-
+w_dr <- paste0("C:/Users/Chris van Schalkwyk/Desktop/RStudio_GitHub/RStudio-Personal/")
+project_dr <- paste0(w_dr, "ChatGPT_Experiments/01_PracticeProject/")
+data_path <- paste0("C:/Users/Chris van Schalkwyk/Desktop/RStudio_GitHub/Data/01_PracticeProject/")
 # List of packages you want to load
 packages <- c("tidyverse", "fst")
 
--- ChatGPT_Experiments/01_PracticeProject/01_data/datapull.R
+++ ChatGPT_Experiments/01_PracticeProject/01_data/datapull.R
@@ -1,5 +1,17 @@
 source("C:/Users/Chris van Schalkwyk/Desktop/RStudio_GitHub/RStudio-Personal/ChatGPT_Experiments/01_PracticeProject/0.packages.R")
 
 
+if(file.exists(file.path(paste0(data_path, "HousePriceData.csv")))){
+  # read CSV file into a data frame
+  data_df <- read_csv(paste0(data_path, "HousePriceData.csv"))
+}else{
+  # URL of the CSV file
+  csv_url <- "https://data.ct.gov/api/views/5mzw-sjtu/rows.csv?accessType=DOWNLOAD"
+  
+  # Download and read the CSV file into a data frame
+  data_df <- read_csv(csv_url)
+}
+
+
 
 
